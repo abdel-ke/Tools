7d52d388cd273b4f9d1aca4d2ac528e76c59e974
abdel-ke <kemmal1337@gmail.com>
2021-08-15 23:34:55 +0100

    ADD_powerlvl10k

---
+++ powerlvl10.sh
@@ -0,0 +1,4 @@
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i "s/ZSH_THEME="powerlevel10k/powerlevel10k/g" ~/.zshrc
